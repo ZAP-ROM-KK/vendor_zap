f3b549627ca08ca39409ae5d601d34455ef14b1e
timpohladthomas <timpohladthomas@gmail.com>
2014-05-05 18:59:35 -0400

    New Theme Engine

--- config/common.mk
+++ config/common.mk
@@ -139,14 +139,14 @@ PRODUCT_PACKAGES += \
     fsck.exfat \
     mkfs.exfat
 
-#ThemeManager
+# Theme engine
 PRODUCT_PACKAGES += \
-       ThemeManager \
-       ThemeChooser \
-       com.tmobile.themes
+    ThemeChooser \
+    ThemesProvider
 
 PRODUCT_COPY_FILES += \
-       vendor/ose/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
+    vendor/ose/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
+    vendor/ose/config/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
 
 # easy way to extend to add more packages
 -include vendor/extra/product.mk
